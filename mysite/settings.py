23df0b2cc2e8651404863d36691daf3a5697159e
Divyesh Nandlal Vishwakarma <divyesh1099@gmail.com>
2021-02-20 22:23:12 +0530

    Css Remaining

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,6 +115,13 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Email Configuration.
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'scamop420@gmail.com'
+EMAIL_HOST_PASSWORD = 'Divyesh123*'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
